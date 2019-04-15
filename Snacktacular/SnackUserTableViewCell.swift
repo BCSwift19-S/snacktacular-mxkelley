f3564386fa426c8aada53e75c8688ed6791bcbf6
Michael Kelley <mx@kell4.com>
2019-04-15 18:23:21 -0400

    9.28 Complete

--- Snacktacular/SnackUserTableViewCell.swift
+++ Snacktacular/SnackUserTableViewCell.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Firebase
+import SDWebImage
 
 private let dateFormatter: DateFormatter = {
     let dateFormatter = DateFormatter()
@@ -25,6 +26,10 @@ class SnackUserTableViewCell: UITableViewCell {
     
     var snackUser: SnackUser! {
         didSet {
+            
+            photoImage.layer.cornerRadius = photoImage.frame.size.width / 2
+            photoImage.clipsToBounds = true
+            
             displayNameLabel.text = snackUser.displayName
             emailLabel.text = snackUser.email
             //Need to include these extra lines due to crashing due to Date() in convenience initializer - Not compatible with Firebase
@@ -35,6 +40,13 @@ class SnackUserTableViewCell: UITableViewCell {
             let formattedDate = dateFormatter.string(from: date as Date)
             //
             userSinceLabel.text = "\(formattedDate)"
+            
+            guard let url = URL(string: snackUser.photoURL) else {
+                photoImage.image = UIImage(named: "singleUser")
+                print("😡 ERROR: Could not convert photoURL named \(snackUser.photoURL) into a valid URL.")
+                return
+            }
+            photoImage.sd_setImage(with: url, placeholderImage: UIImage(named: "singleUser"))
         }
     }
 }
